643bb4eef92bd4ff2a7fb8a6c8d63fbcb855350b
Nandy R. Gulle Jr <nandygullejr@gmail.com>
2019-12-21 23:22:38 +0800

    Runtime: 72 ms, faster than 46.17% of C# online submissions for Palindrome Number.

---
+++ IsPalindrome.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool IsPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        var temporary = x;
+        var reverse = 0;
+        
+        while (x > 0) {
+            reverse = (reverse * 10) + (x % 10);
+            x = x / 10;
+        }
+
+        return (temporary == reverse);
+    }
+}
