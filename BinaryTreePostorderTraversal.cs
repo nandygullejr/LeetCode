1c4e849af7bbb38de33ec4c933f59860dcb68383
Nanan <nandygullejr@yahoo.com>
2020-02-01 17:38:12 +0800

    Runtime beats 83.86% of csharp submissions.

--- BinaryTreePostorderTraversal.cs
+++ BinaryTreePostorderTraversal.cs
@@ -8,21 +8,19 @@
  * }
  */
 public class Solution {
+    IList<int> iList = new List<int>(){};
+    
     public IList<int> PostorderTraversal(TreeNode root) {
-        IList<int> iList = new List<int>(){};
-        Stack stack = new Stack();
-        
-        while ((root != null) || (stack.Count > 0)) {
-            if (root != null) {
-                stack.Push(root);
-                iList.Insert(0, root.val);
-                root = root.right;
-            } else {
-                root = stack.Pop() as TreeNode;
-                root = root.left;
+        if (root != null) {
+            if (root.left != null) {
+                PostorderTraversal(root.left);
             }
-        }       
-        
+            if (root.right != null) {
+                PostorderTraversal(root.right);
+            }
+            iList.Add(root.val);
+        }
+        // Base case: if root == null, iList will be empty
         return iList;
     }
 }
